c5989191a94c7b01aee143b1496aae60a0d9abb6
Trapp <luciane.trapp@accenture.com>
2022-05-05 22:13:58 -0300

    aula dia 05-05 - rest

---
+++ examples-code/rest-camel-app/src/main/java/org/camel/app/bean/MovieBean.java
@@ -0,0 +1,25 @@
+package org.camel.app.bean;
+
+import org.apache.camel.Exchange;
+import org.camel.app.model.Movie;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieBean {
+
+    public void getMovies(Exchange exchange){
+        List<Movie> movies = new ArrayList<>();
+
+        for(int a=0; a<=10; a++){
+            Movie m = new Movie();
+            m.setNome("Filme ".concat(String.valueOf(a)));
+            m.setGenero("Comédia");
+
+            movies.add(m);
+        }
+
+        exchange.getMessage().setBody(movies);
+    }
+}
---
+++ examples-code/rest-camel-app/src/main/java/org/camel/app/model/Movie.java
@@ -0,0 +1,24 @@
+package org.camel.app.model;
+
+
+public class Movie {
+
+    private String nome;
+    private String genero;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
